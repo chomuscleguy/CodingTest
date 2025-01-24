bf6cb9cdcbaaa0349da4b842e498313494fa18b8
Robert CHO <145965718+chomuscleguy@users.noreply.github.com>
2025-01-24 16:31:43 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ C++/프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> num_list) 
+{
+    int odd = 0;
+    int even = 0;
+
+    for (int num : num_list) 
+    {
+        if (num % 2 == 0)
+            even = even * 10 + num;
+        else 
+            odd = odd * 10 + num;
+    }
+
+    return odd + even;
+}
