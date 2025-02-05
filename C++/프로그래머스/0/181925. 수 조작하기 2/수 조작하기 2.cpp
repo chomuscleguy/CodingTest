5cd562f24ffb1221441ecac73846587d65144986
Robert CHO <145965718+chomuscleguy@users.noreply.github.com>
2025-02-05 17:05:32 +0900

    [level 0] Title: 수 조작하기 2, Time: 1.36 ms, Memory: 6.83 MB -BaekjoonHub

---
+++ C++/프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<int> numLog) 
+{
+    string answer = "";
+    for (int i = 1; i < numLog.size(); i++)
+    {
+        switch (numLog[i]-numLog[i-1])
+        {
+        case -10 :
+            answer += 'a';
+            break;
+        case -1:
+            answer += 's';
+            break;
+        case 10:
+            answer += 'd';
+            break;
+        case 1:
+            answer += 'w';
+            break;
+        }
+    }
+    return answer;
+}
