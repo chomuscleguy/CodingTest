fd74d79017f48a4fea5c2fd03a8a90c79d57303c
Robert CHO <145965718+chomuscleguy@users.noreply.github.com>
2025-02-12 11:03:45 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ C++/프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+
+using namespace std;
+
+string solution(string my_string, int s, int e)
+{
+	string answer = my_string.substr(0, s);
+
+	for (int i = e; i >= s; i--)
+		answer += my_string[i];
+
+	answer += my_string.substr(e + 1);
+
+	return answer;
+}
