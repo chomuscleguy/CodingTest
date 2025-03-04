1c51d14792b63bb1e1bda0a7c80b9c1e59918e24
Robert CHO <145965718+chomuscleguy@users.noreply.github.com>
2025-03-04 17:04:36 +0900

    [level 0] Title: l로 만들기, Time: 0.41 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ C++/프로그래머스/0/181834. l로 만들기/l로 만들기.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string myString) 
+{
+    for (char& ch : myString)
+        if (ch < 'l')
+            ch = 'l';
+    return myString;
+}
