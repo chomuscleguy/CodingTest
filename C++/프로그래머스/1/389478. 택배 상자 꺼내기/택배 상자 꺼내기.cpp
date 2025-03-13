184aaa1aa8ae9802d951480c1a6f487f510044e0
Robert CHO <145965718+chomuscleguy@users.noreply.github.com>
2025-03-13 14:10:44 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.02 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ C++/프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.cpp
@@ -0,0 +1,12 @@
+using namespace std;
+
+int solution(int n, int w, int num) 
+{
+    int answer = 0;
+    while (num <= n) 
+    {
+        num += 2 * (w - ((num - 1) % w) - 1) + 1;
+        answer++;
+    }
+    return answer;
+}
