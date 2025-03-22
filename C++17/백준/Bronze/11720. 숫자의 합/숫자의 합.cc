ba939ca247c2d69745f189f72a588c2b98ee899d
Robert CHO <145965718+chomuscleguy@users.noreply.github.com>
2025-03-23 01:11:32 +0900

    [Bronze IV] Title: 숫자의 합, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/11720. 숫자의 합/숫자의 합.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	int n , result = 0;
+	string str;
+
+	cin >> n >> str;
+
+	for (int i = 0; i < n; i++)
+		result += str[i] - '0';
+
+	cout << result;
+
+	return 0;
+}
