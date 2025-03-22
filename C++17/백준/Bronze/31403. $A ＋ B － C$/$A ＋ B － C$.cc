f1e5b7ec71915c1960fd510a7f8b57fcf8e5ac20
Robert CHO <145965718+chomuscleguy@users.noreply.github.com>
2025-03-23 01:35:42 +0900

    [Bronze IV] Title: $A + B - C$, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/31403. $A ＋ B － C$/$A ＋ B － C$.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int A, B, C;
+
+    cin >> A >> B >> C;
+
+    cout << A + B - C << "\n";
+
+    cout << stoi(to_string(A) + to_string(B)) - C;
+
+    return 0;
+}
