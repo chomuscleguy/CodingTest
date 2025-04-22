1fdec93320afe818c54400f723c304378737f419
Robert CHO <145965718+chomuscleguy@users.noreply.github.com>
2025-04-22 10:13:01 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int N;
+	cin >> N;
+
+	int dp[1001] = { 0 };
+
+	dp[1] = 1, dp[2] = 2;
+
+	for (int i = 3; i <= 1000; i++)
+		dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+
+	cout << dp[N] << "\n";
+
+	return 0;
+}
