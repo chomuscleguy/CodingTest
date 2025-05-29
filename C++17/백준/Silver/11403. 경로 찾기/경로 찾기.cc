ca9f0b598021ca7de3eadfe3292689b2e6bac00f
Robert CHO <145965718+chomuscleguy@users.noreply.github.com>
2025-05-29 23:20:43 +0900

    [Silver I] Title: 경로 찾기, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- C++17/백준/Silver/11403. 경로 찾기/경로 찾기.cc
+++ C++17/백준/Silver/11403. 경로 찾기/경로 찾기.cc
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <vector>
-#include <queue>
 
 using namespace std;
 
@@ -9,41 +8,22 @@ int main()
 	int N;
 	cin >> N;
 
-	vector<vector<int>> graph(N, vector<int>(N));
-	vector<vector<int>> result(N, vector<int>(N, 0));
+	vector<vector<int>> board(N, vector<int>(N));
 
 	for (int i = 0; i < N; i++)
 		for (int j = 0; j < N; j++)
-			cin >> graph[i][j];
+			cin >> board[i][j];
 
 	for (int k = 0; k < N; k++)
-	{
-		vector<bool> visited(N, false);
-		queue<int> q;
-
-		q.push(k);
-
-		while (!q.empty())
-		{
-			int curr = q.front();
-			q.pop();
-
-			for (int next = 0; next < N; next++)
-			{
-				if (graph[curr][next] == 1 && !visited[next])
-				{
-					visited[next] = true;
-					result[k][next] = 1;
-					q.push(next);
-				}
-			}
-		}
-	}
+		for (int i = 0; i < N; i++)
+			for (int j = 0; j < N; j++)
+				if (board[i][k] && board[k][j])
+					board[i][j] = 1;
 
 	for (int i = 0; i < N; i++)
 	{
 		for (int j = 0; j < N; j++)
-			cout << result[i][j] << " ";
+			cout << board[i][j] << " ";
 		cout << '\n';
 	}
 
