f8e2f461367f9c05b6d76e71e17eada3d2090723
Robert CHO <145965718+chomuscleguy@users.noreply.github.com>
2025-08-25 17:04:55 +0900

    [Bronze IV] Title: 달달함이 넘쳐흘러, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/17256. 달달함이 넘쳐흘러/달달함이 넘쳐흘러.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a[3], b[3], c[3];
+
+	for (int i = 0; i < 3; i++)
+		cin >> a[i];
+	for (int i = 0; i < 3; i++)
+		cin >> c[i];
+
+	b[0] = c[0] - a[2];
+	b[1] = c[1] / a[1];
+	b[2] = c[2] - a[0];
+
+	cout << b[0] << " " << b[1] << " " << b[2] << "\n";
+
+	return 0;
+}
