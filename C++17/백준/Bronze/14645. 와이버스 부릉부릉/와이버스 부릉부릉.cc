da21f0b851d0185694c3820c68ca974d740ad1cf
Robert CHO <145965718+chomuscleguy@users.noreply.github.com>
2025-08-26 13:04:09 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int N, K;
+
+	cin >> N >> K;
+
+	while (N-- && cin >> K >> K)
+		continue;
+
+	cout << "비와이" << '\n';
+
+	return 0;
+}
