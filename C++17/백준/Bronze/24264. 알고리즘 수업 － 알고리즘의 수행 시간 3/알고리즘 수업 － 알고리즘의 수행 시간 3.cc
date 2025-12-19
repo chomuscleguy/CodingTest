16146b151b443f4c2046e9115884b6d72a42a048
Robert CHO <145965718+chomuscleguy@users.noreply.github.com>
2025-12-19 15:29:14 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.cc
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	long long n;
+	cin >> n;
+
+	cout << n * n << '\n' << 2 << '\n';
+
+	return 0;
+}
