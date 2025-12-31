7226aab6d22f2fb2b90be0b5e768fc8d775faa0f
Robert CHO <145965718+chomuscleguy@users.noreply.github.com>
2026-01-01 01:28:00 +0900

    [Bronze V] Title: 홍익대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/16394. 홍익대학교/홍익대학교.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int32_t N;
+	cin >> N;
+
+	cout << N - 1946 << '\n';
+
+	return 0;
+}
