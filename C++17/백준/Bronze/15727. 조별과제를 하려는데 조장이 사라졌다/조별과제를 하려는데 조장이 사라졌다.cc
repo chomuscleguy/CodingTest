2ef1c29978e2960acffd5036e1204f46cd6696b7
Robert CHO <145965718+chomuscleguy@users.noreply.github.com>
2026-03-04 20:25:12 +0900

    [Bronze V] Title: 조별과제를 하려는데 조장이 사라졌다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/15727. 조별과제를 하려는데 조장이 사라졌다/조별과제를 하려는데 조장이 사라졌다.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int L;
+	cin >> L;
+
+	cout << (L + 4) / 5 << '\n';
+
+	return 0;
+}
