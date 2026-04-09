276986e58336f114eee6b8ed5111e4f6a0f01df0
Robert CHO <145965718+chomuscleguy@users.noreply.github.com>
2026-04-09 18:42:33 +0900

    [Bronze V] Title: 나부 함대 데이터, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- C++17/백준/Bronze/9654. 나부 함대 데이터/나부 함대 데이터.cc
+++ C++17/백준/Bronze/9654. 나부 함대 데이터/나부 함대 데이터.cc
@@ -1,15 +1,20 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
 int main()
 {
-    cout << "SHIP NAME      CLASS          DEPLOYMENT IN SERVICE\n";
-    cout << "N2 Bomber      Heavy Fighter  Limited    21        \n";
-    cout << "J-Type 327     Light Combat   Unlimited  1         \n";
-    cout << "NX Cruiser     Medium Fighter Limited    18        \n";
-    cout << "N1 Starfighter Medium Fighter Unlimited  25        \n";
-    cout << "Royal Cruiser  Light Combat   Limited    4         \n";
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	string str1[] = { "SHIP NAME","N2 Bomber","J-Type 327","NX Cruiser","N1 Starfighter","Royal Cruiser" };
+	string str2[] = { "CLASS","Heavy Fighter","Light Combat","Medium Fighter","Medium Fighter","Light Combat" };
+	string str3[] = { "DEPLOYMENT","Limited","Unlimited","Limited","Unlimited","Limited" };
+	string str4[] = { "IN SERVICE","21","1","18","25","4" };
+
+	for (int i = 0;i < 6;i++)
+		cout << left << setw(15) << str1[i] << setw(15) << str2[i] << setw(11) << str3[i] << setw(10) << str4[i] << '\n';
 
 	return 0;
 }
