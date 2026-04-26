3c7b5d20f53cc85a8d2688cb99719434fb623df5
Robert CHO <145965718+chomuscleguy@users.noreply.github.com>
2026-04-26 13:28:43 +0900

    [Bronze II] Title: 텔레프라임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/7789. 텔레프라임/텔레프라임.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+bool isPrime(int n)
+{
+	if (n < 2)
+		return false;
+
+	for (int i = 2; i * i <= n; i++)
+	{
+		if (n % i == 0)
+			return false;
+	}
+
+	return true;
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int curNum, addNum;
+
+	cin >> curNum >> addNum;
+
+	int newNeoNum = curNum + addNum * 1000000;
+
+	cout << (isPrime(curNum) && isPrime(newNeoNum) ? "Yes" : "No") << '\n';
+
+	return 0;
+}
