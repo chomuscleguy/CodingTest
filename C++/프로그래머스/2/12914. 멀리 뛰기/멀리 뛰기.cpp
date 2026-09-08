e06e7239e1208bb1698cfbba175503fb96e0c71a
Robert CHO <145965718+chomuscleguy@users.noreply.github.com>
2026-09-08 09:54:48 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.02 ms, Memory: 4.82 MB -BaekjoonHub

---
+++ C++/프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+long long dp[2001] = { 1,2 };
+
+long long solution(int n)
+{
+	for (int i = 2;i < n;i++)
+		dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
+
+	return dp[n - 1];
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	cout << solution(n) << '\n';
+
+	return 0;
+}
